f50ef4bdedd471f14acf490a3e7b3a06699cfe1e
liveworldstrike <lazodellamas12@gmail.com>
2016-01-25 13:09:39 +0100

    añadido metodo set,get para discografica

--- Track.java
+++ Track.java
@@ -15,19 +15,24 @@ public class Track
     private String filename;
     //cuenta las veces que se reproduce una cancion
     private int playCount;
-    
+    //discobrafica a la que pertenece 
+    private String discografica;
+
     /**
      * Constructor for objects of class Track.
      * @param artist The track's artist.
      * @param title The track's title.
      * @param filename The track file. 
      */
-    public Track(String artist, String title, String filename)
+    public Track(String artist, String title, String filename, String nombreDiscografica)
     {
         setDetails(artist, title, filename);
+        setDiscografica(nombreDiscografica);
         playCount = 0;
+        
+
     }
-    
+
     /**
      * Constructor for objects of class Track.
      * It is assumed that the file name cannot be
@@ -38,7 +43,7 @@ public class Track
     {
         setDetails("unknown", "unknown", filename);
     }
-    
+
     /**
      * Return the artist.
      * @return The artist.
@@ -47,7 +52,7 @@ public class Track
     {
         return artist;
     }
-    
+
     /**
      * Return the title.
      * @return The title.
@@ -56,7 +61,7 @@ public class Track
     {
         return title;
     }
-    
+
     /**
      * Return the file name.
      * @return The file name.
@@ -65,16 +70,24 @@ public class Track
     {
         return filename;
     }
-        
+
+    /**
+     * devuelve la discografica 
+     */
+    public String getDiscografica()
+    {
+        return discografica ;
+    }
+
     /**
      * Return details of the track: artist, title and file name.
      * @return The track's details.
      */
     public String getDetails()
     {
-        return artist + ": " + title + "  (file: " + filename + ")" + playCount ;
+        return artist + ": " + title + "  (file: " + filename + " discografica "+ discografica + ")" + playCount ;
     }
-    
+
     /**
      * Set details of the track.
      * @param artist The track's artist.
@@ -86,24 +99,35 @@ public class Track
         this.artist = artist;
         this.title = title;
         this.filename = filename;
+        
     }
-    
+
     /**
      * metodo para resetear playCount 
      */
     public  void  resetCount()
     {
         playCount = 0;
-    
+
     }
-    
+
     /**
      * metodo para incrementar en uno  playCount 
      */
     public void  incrementCount()
     {
         playCount = playCount + 1;
-    
+
     }
+
+    /**
+     * metodo para dar discografica a una cancion 
+     */
+    public void setDiscografica(String nombreDiscografica)
+    {
+        this.discografica = nombreDiscografica ;
+
+    }
+
     
 }
